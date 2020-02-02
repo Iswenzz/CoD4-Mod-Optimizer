89ae4eb410d3b441d5e99197f0304d9c076f7236
iswenzz <suxlolz1528@gmail.com>
2020-02-02 12:41:31 +0100

    main: removed global variables and refactor

--- main.py
+++ main.py
@@ -7,28 +7,28 @@ from csv_file import CSV
 import os
 import sys
 
-in_p = "in"
-out_p = "out"
+def parseCSV(in_path = None, out_path = None):
 
-def parseCSV():
+	if not os.path.exists(Path(out_path) / "csv/csv_material.txt"):
+		with open(Path(out_path) / "csv/csv_material.txt", "w"):
+			pass
+	if not os.path.exists(Path(out_path) / "csv/csv_xmodel.txt"):
+		with open(Path(out_path) / "csv/csv_xmodel.txt", "w"):
+			pass
+	if not os.path.exists(Path(out_path) / "csv/csv_material_all.txt"):
+		with open(Path(out_path) / "csv/csv_material_all.txt", "w"):
+			pass
 
-	if not os.path.exists(Path(out_p) / "csv/csv_material.txt"):
-		with open(Path(out_p) / "csv/csv_material.txt", "w"): pass
-	if not os.path.exists(Path(out_p) / "csv/csv_xmodel.txt"):
-		with open(Path(out_p) / "csv/csv_xmodel.txt", "w"): pass
-	if not os.path.exists(Path(out_p) / "csv/csv_material_all.txt"):
-		with open(Path(out_p) / "csv/csv_material_all.txt", "w"): pass
-
-	csv_input = CSV(Path(in_p) / "mod.csv")
+	csv_input = CSV(Path(in_path) / "mod.csv")
 	csv_input.parse(csv_input.csv_path, useAssetType = False)
 
-	with open(Path(out_p) / "csv/csv_material.txt", "w") as f:
+	with open(Path(out_path) / "csv/csv_material.txt", "w") as f:
 		f.writelines(csv_input.csv_assets["material"])
-	with open(Path(out_p) / "csv/csv_xmodel.txt", "w") as f:
+	with open(Path(out_path) / "csv/csv_xmodel.txt", "w") as f:
 		f.writelines(csv_input.csv_assets["xmodel"])
 
-	with open(Path(out_p) / "csv/csv_material_all.txt", "w") as f:
-		for _, _, files in os.walk(Path(in_p) / "materials", topdown = False):
+	with open(Path(out_path) / "csv/csv_material_all.txt", "w") as f:
+		for _, _, files in os.walk(Path(in_path) / "materials", topdown = False):
 			for name in files:
 				f.write(name + "\n")
 
@@ -39,74 +39,58 @@ def delete(path):
 		os.remove(path)
 
 
-def checkPath():
+def checkPath(in_path = None, out_path = None):
 
-	if not os.path.exists(Path(in_p)):
-		print("Folder doesn't exists: " + str(Path(in_p)))
-		sys.exit(-1)
-	if not os.path.exists(Path(in_p) / "materials"):
-		print("Folder doesn't exists: " + str(Path(in_p) / "materials"))
-		sys.exit(-1)
-	if not os.path.exists(Path(in_p) / "xmodel"):
-		print("Folder doesn't exists: " + str(Path(in_p) / "xmodel"))
-		sys.exit(-1)
-	if not os.path.exists(Path(in_p) / "images"):
-		print("Folder doesn't exists: " + str(Path(in_p) / "images"))
-		sys.exit(-1)
-	if not os.path.exists(Path(in_p) / "mod.csv"):
-		print("mod.csv doesn't exists: " + str(Path(in_p) / "mod.csv"))
-		sys.exit(-1)
+	in_check = ["materials", "xmodel", "images"]
+	out_check = ["materials", "xmodel", "images", "csv"]
 
-	if not os.path.exists(Path(out_p)):
-		os.mkdir(Path(out_p))
-	if not os.path.exists(Path(out_p) / "materials"):
-		os.mkdir(Path(out_p) / "materials")
-	if not os.path.exists(Path(out_p) / "xmodel"):
-		os.mkdir(Path(out_p) / "xmodel")
-	if not os.path.exists(Path(out_p) / "images"):
-		os.mkdir(Path(out_p) / "images")
-	if not os.path.exists(Path(out_p) / "csv"):
-		os.mkdir(Path(out_p) / "csv")
+	for f in in_check:
+		if not os.path.exists(Path(in_path) / f):
+			os.mkdir(Path(in_path) / f)
+
+	for f in out_check:
+		if not os.path.exists(Path(out_path) / f):
+			os.mkdir(Path(out_path) / f)
+
+	if not os.path.exists(Path(in_path) / "mod.csv"):
+		print("ERROR: File " + str(Path(in_path) / "mod.csv") + " not found.")
+		sys.exit(-1)
 
 
 def main():
 
-	global in_p
-	global out_p
+	in_p = "in"
+	out_p = "out"
 
 	if len(sys.argv) == 3:
 		in_p = sys.argv[1]
 		out_p = sys.argv[2]
 	else:
-		print("Copyright (c) Iswenzz 2018-2019\n\n"
-			+ "Usage: IWD Optimizer.exe \"C:/cod4/mod/name\" \"C:/cod4/mod/name/opti\"\n"
-			+ "\t<input path> <output path> (default path is: EXE_DIR/in, EXE_DIR/out)\n\n")
+		print("""
+Copyright (c) Iswenzz 2018-2020
+
+Usage: IWD Optimizer.exe "C:/cod4/mod/name" "C:/cod4/mod/name/opti"
+	<input path> <output path> (default path is: EXE_DIR/in, EXE_DIR/out)\n
+""")
 
 	print("Input PATH: " + in_p + "\nOutput PATH: " + out_p + "\n")
 
-	convert()
+	convert(in_p, out_p)
 
 
 def convert(in_path = None, out_path = None):
 
-	if in_path is not None:
-		global in_p
-		in_p = in_path
-	if out_path is not None:
-		global out_p
-		out_p = out_path
+	checkPath(in_path, out_path)
 
-	checkPath()
+	Materials = MaterialAsset(in_path, out_path)
+	XModels = XmodelAsset(in_path, out_path)
+	Images = ImageAsset(in_path, out_path)
 
-	Materials = MaterialAsset(in_p, out_p)
-	XModels = XmodelAsset(in_p, out_p)
-	Images = ImageAsset(in_p, out_p)
-
-	delete(Path(out_p) / "csv/csv_material.txt")
-	delete(Path(out_p) / "csv/csv_material_all.txt")
-	delete(Path(out_p) / "csv/csv_xmodel.txt")
-	delete(Path(out_p) / "images_list.txt")
-	delete(Path(out_p) / "xmodel_material_list.txt")
+	delete(Path(out_path) / "csv/csv_material.txt")
+	delete(Path(out_path) / "csv/csv_material_all.txt")
+	delete(Path(out_path) / "csv/csv_xmodel.txt")
+	delete(Path(out_path) / "images_list.txt")
+	delete(Path(out_path) / "xmodel_material_list.txt")
 
 	Materials.delete()
 	XModels.delete()
@@ -114,27 +98,33 @@ def convert(in_path = None, out_path = None):
 
 	print("Parsing CSV...")
 
-	parseCSV()
+	parseCSV(in_path, out_path)
 
-	print("\n#####################################\n"
-            + "               XModels\n"
-            + "#####################################\n")
+	print("""
+#####################################
+              XModels
+#####################################
+""")
 
 	XModels.loadAssets()
 	XModels.move(XModels.out_path)
 	XModels.convert()
 
-	print("\n#####################################\n"
-            + "              Materials\n"
-            + "#####################################\n")
+	print("""
+#####################################
+             Materials
+#####################################
+""")
 
 	Materials.loadAssets()
 	Materials.move(Materials.out_path)
 	Materials.convert()
 
-	print("\n#####################################\n"
-            + "               Images\n"
-            + "#####################################\n")
+	print("""
+#####################################
+               Images
+#####################################
+""")
 
 	Images.loadAssets()
 	Images.move(Images.out_path)
